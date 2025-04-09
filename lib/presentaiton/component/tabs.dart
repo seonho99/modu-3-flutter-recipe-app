a77b099c4835d6925817a2a9fcacde77e1ca1ff8
seonho99 <pandapad10@gmail.com>
2025-04-09 19:20:54 +0900

    feat: TabWidget 구현

--- lib/presentaiton/component/tabs.dart
+++ lib/presentaiton/component/tabs.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/presentaiton/ui/color_styles.dart';
-
 import '../ui/text_styles.dart';
 
 void main() {
@@ -15,24 +14,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+
+  void tabSelected(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body:,
+        backgroundColor: ColorStyles.primary40,
+        body: Center(
+          child: TabsWidget(
+            labels: ['Label', 'Label'],
+            selectedIndex: selectedIndex,
+            onValueChange: tabSelected,
+          ),
+        ),
       ),
     );
   }
 }
 
-
-class TabsWidget extends StatelessWidget implements TextStyles {
+class TabsWidget extends StatelessWidget {
   final List<String> labels;
   final int selectedIndex;
   final Function(int) onValueChange;
 
   const TabsWidget({
+    super.key,
     required this.labels,
     required this.selectedIndex,
     required this.onValueChange,
@@ -40,13 +53,38 @@ class TabsWidget extends StatelessWidget implements TextStyles {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 375,
-        height: 58,
-        child:
+    return Container(
+      width: 375,
+      height: 58,
+      padding: EdgeInsets.fromLTRB(30, 12, 30, 13),
+      color: Colors.white,
+      child: Row(
+        children: List.generate(labels.length, (index) {
+          final isSelected = index == selectedIndex;
 
-
+          return Row(
+            children: [
+              GestureDetector(
+                onTap: () => onValueChange(index),
+                child: Container(
+                  width: 150,
+                  height: 33,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: isSelected ? ColorStyles.primary100 : Colors.transparent,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    labels[index],
+                    style: isSelected
+                        ? TextStyles.smallerTextBold.copyWith(color: Colors.white)
+                        : TextStyles.smallerTextBold.copyWith(color: ColorStyles.primary100),
+                  ),
+                ),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
